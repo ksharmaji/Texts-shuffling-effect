f814566ac46fc8db031fa6fb1cd1cceaefd9015b
ksharmaji <rohit79807@gmail.com>
2026-04-20 16:23:32 +0530

    firstcommit

---
+++ script.js
@@ -0,0 +1,22 @@
+let para = document.querySelector(".main p");
+let text =para.innerText;
+let surprise = document.querySelector(".main h2");
+let surp =surprise.innerText;
+surprise.style.display = "none";
+const charac = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+let interation = 0;
+
+para.addEventListener('mouseenter',()=>{
+function randomText(){
+    str = surp.split("").map((char, index)=>{
+        if(index < interation){
+            return char;
+        }
+        return charac[Math.floor(Math.random()*charac.length)]
+    }).join('')
+    para.innerText = str;
+    interation += 0.2;
+}
+setInterval(randomText,30)
+
+});
